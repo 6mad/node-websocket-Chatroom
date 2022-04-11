f6c7068c12bd7fcb243d248d26654b52080c9b5d
李昊 <lihao@sohh.cn>
2022-04-11 17:30:23 +0800

    完成xsbb评论测试api

--- server/index.js
+++ server/index.js
@@ -13,11 +13,11 @@ server.listen(PORT,()=> {
   console.info(`- Network: http://${address}:`+PORT)
 });
 
-app.get('/test',(req,res)=>{
+app.get('/api/test',(req,res)=>{
 	res.send("<h1>test666</h1>")
 })
 
-app.get('/questionslist/time',(req,res)=>{
+app.get('/api/questionslist/time',(req,res)=>{
 	cardData = ()=> {
 		return {
         title: 'ss',
@@ -42,8 +42,88 @@ app.get('/questionslist/time',(req,res)=>{
 		  'total':1
 	  }
 	  console.log('req:'+req.param[0]);
+	  console.log('req:'+req.publicPath);
 	res.send(result)
 })
-// app.get('/questionslist/time',(req,res)=>{
-// 	res.send("12")
-// })
+app.get('/api/commentsdata',(req,res)=>{
+	let comments = [
+	                {
+	                    name:'Lana Del Rey',
+	                    id:19870621,
+	                    headImg:'https://ae01.alicdn.com/kf/Hd60a3f7c06fd47ae85624badd32ce54dv.jpg',
+	                    comment:'我发布一张新专辑Norman Fucking Rockwell,大家快来听啊',
+	                    time:'2019年9月16日 18:43',
+	                    commentNum:2,
+	                    like:15,
+	                    inputShow:false,
+	                    reply:[
+	                        {
+	                            from:'Taylor Swift',
+	                            fromId:19891221,
+	                            fromHeadImg:'https://ae01.alicdn.com/kf/H94c78935ffa64e7e977544d19ecebf06L.jpg',
+	                            to:'Lana Del Rey',
+	                            toId:19870621,
+	                            comment:'我很喜欢你的新专辑！！',
+	                            time:'2019年9月16日 18:43',
+	                            commentNum:1,
+	                            like:15,
+	                            inputShow:false
+	                        },
+	                        {
+	                            from:'Ariana Grande',
+	                            fromId:1123,
+	                            fromHeadImg:'https://ae01.alicdn.com/kf/Hf6c0b4a7428b4edf866a9fbab75568e6U.jpg',
+	                            to:'Lana Del Rey',
+	                            toId:19870621,
+	                            comment:'别忘记宣传我们的合作单曲啊',
+	                            time:'2019年9月16日 18:43',
+	                            commentNum:0,
+	                            like:5,
+	                            inputShow:false
+	
+	                        }
+	                    ]
+	                },
+	                {
+	                    name:'Taylor Swift',
+	                    id:19891221,
+	                    headImg:'https://ae01.alicdn.com/kf/H94c78935ffa64e7e977544d19ecebf06L.jpg',
+	                    comment:'我发行了我的新专辑Lover',
+	                    time:'2019年9月16日 18:43',
+	                    commentNum:1,
+	                    like:5,
+	                    inputShow:false,
+	                    reply:[
+	                        {
+	                            from:'Lana Del Rey',
+	                            fromId:19870621,
+	                            fromHeadImg:'https://ae01.alicdn.com/kf/Hd60a3f7c06fd47ae85624badd32ce54dv.jpg',
+	                            to:'Taylor Swift',
+	                            toId:19891221,
+	                            comment:'新专辑和speak now 一样棒！',
+	                            time:'2019年9月16日 18:43',
+	                            commentNum:25,
+	                            like:5,
+	                            inputShow:false
+	
+	                        }
+	                    ]
+	                },
+	                {
+	                    name:'Norman Fucking Rockwell',
+	                    id:20190830,
+	                    headImg:'https://ae01.alicdn.com/kf/Hdd856ae4c81545d2b51fa0c209f7aa28Z.jpg',
+	                    comment:'Plz buy Norman Fucking Rockwell on everywhere',
+	                    time:'2019年9月16日 18:43',
+	                    commentNum:0,
+	                    like:5,
+	                    inputShow:false,
+	                    reply:[]
+	                },
+	            ]
+	res.send(comments)
+})
+app.get('/api/questionslist/total',(req,res)=>{
+	console.log('api/questionslist/total')
+	res.send("12")
+})
